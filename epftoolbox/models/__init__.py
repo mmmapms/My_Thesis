2baaa8b6caf110499c41db64563595f9c70169a4
mmmapms <33175792+mmmapms@users.noreply.github.com>
2022-05-27 18:09:03 +0100

    Delete epftoolbox directory

--- epftoolbox/models/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from ._lear import (LEAR, evaluate_lear_in_test_dataset)
-from ._lear_lago import LEAR_old
-from ._dnn_FELIX import (DNNModel, DNN, evaluate_dnn_in_test_dataset, format_best_trial)
-from ._dnn_hyperopt import (hyperparameter_optimizer)
-from ._utils import (ScalingTypes, FeatureLags)
